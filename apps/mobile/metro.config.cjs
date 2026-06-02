842f3ffa57b67563f279669cadfbccf7bac00f1f
maxassis <max.assis@ymail.com>
2026-06-02 11:30:30 -0400

    metro nao observa mais api

--- apps/mobile/metro.config.cjs
+++ apps/mobile/metro.config.cjs
@@ -1,10 +1,19 @@
+const path = require('node:path')
 const { getDefaultConfig } = require('expo/metro-config')
 const { withUniwindConfig } = require('uniwind/metro')
 
+function escapePathForRegex(pathname) {
+  return pathname.replaceAll('\\', '/').replace(/[|\\{}()[\]^$+*?.]/g, '\\$&')
+}
+
 module.exports = (() => {
   const config = getDefaultConfig(__dirname)
+  const apiRoot = path.resolve(__dirname, '../api')
+  const apiRootRegex = new RegExp(`${escapePathForRegex(apiRoot)}/.*`)
 
   const { transformer, resolver } = config
+
+  config.watchFolders = config.watchFolders?.filter(folder => folder !== apiRoot)
 
   config.transformer = {
     ...transformer,
@@ -13,6 +22,11 @@ module.exports = (() => {
   config.resolver = {
     ...resolver,
     assetExts: resolver.assetExts.filter(ext => ext !== 'svg'),
+    blockList: Array.isArray(resolver.blockList)
+      ? [...resolver.blockList, apiRootRegex]
+      : resolver.blockList
+        ? [resolver.blockList, apiRootRegex]
+        : [apiRootRegex],
     sourceExts: [...resolver.sourceExts, 'svg'],
   }
 
