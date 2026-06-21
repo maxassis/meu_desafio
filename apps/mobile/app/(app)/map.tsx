7db19279d47c28d61a7f813fea438a320aa5a6bc
maxassis <max.assis@ymail.com>
2026-06-21 19:15:33 -0300

    ajustado o anchor do pin

--- apps/mobile/app/(app)/map.tsx
+++ apps/mobile/app/(app)/map.tsx
@@ -296,6 +296,7 @@ export default function Map2() {
         <Marker
           key={user.userId}
           coordinate={coordinate}
+          anchor={{ x: 0.5, y: 0.95 }}
           style={{
             zIndex: isCurrentUser ? 50 : index,
             elevation: isCurrentUser ? 50 : index,
@@ -511,6 +512,7 @@ export default function Map2() {
           <Marker
             key="final"
             coordinate={routeCoordinates[routeCoordinates.length - 1]}
+            anchor={{ x: 0.5, y: 0.95 }}
             style={{ zIndex: 40, elevation: 40 }}
             title="Final"
             tracksViewChanges={!markersReady}
